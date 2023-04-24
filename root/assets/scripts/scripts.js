7d3f472f235f3b16de187290ded2168adff9555f
funnydusto <87397069+funnydusto@users.noreply.github.com>
2023-04-23 23:56:32 -0400

    More infrastructure upgrades or smth

--- root/assets/scripts/scripts.js
+++ root/assets/scripts/scripts.js
@@ -1,2 +1,5 @@
-let splashheader = document.getElementById('header');
-splashheader.innerText = 'wooo javascript is fun!!';
+let splashheader = document.getElementById('splashheader');
+let splashsubtitle = document.getElementById('splashsubtitle');
+
+splashheader.innerText = 'Welcome back!';
+splashsubtitle.innerText = 'Check back up on my latest projects, find a product, or learn more about me here.';
