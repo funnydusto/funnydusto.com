b6ee1f9169399e732fe0fdc33c3015e62b533aa9
funnydusto <dustopersonal@gmail.com>
2024-06-14 11:06:21 -0400

    implementing astro & deprecating old src

--- root_legacy/assets/scripts/scripts.js
+++
@@ -1,5 +0,0 @@
-let splashheader = document.getElementById('splashheader');
-let splashsubtitle = document.getElementById('splashsubtitle');
-
-splashheader.innerText = 'Welcome back!';
-splashsubtitle.innerText = 'Check back up on my latest projects, find a product, or learn more about me here.';
