62ab82fcf0be3fc2587a6ca99a00aacf43cd04ac
funnydusto <dustopersonal@gmail.com>
2024-06-16 20:32:11 -0400

    adding data and some stuff

---
+++ src/data/data.ts
@@ -0,0 +1,52 @@
+const Projects = [
+    {
+        label:"chibeeu",
+        subLabel:"founder",
+        description:"adorable chibi avatar on the web",
+        url:"https://chibeeu.com"
+    },
+    {
+        label:"buildspace",
+        subLabel:"n&ws s5",
+        description:"participating in this year's buildspace",
+        url:"https://buildspace.so"
+    },
+    {
+        label:"view more",
+        subLabel:"portfolio",
+        description:"check out commissioned work, small projects, and random doodles",
+        url:"/portfolio"
+    },
+]
+
+const Connections = [
+    {
+        label:"email",
+        subLabel:"funnydustobusiness@gmail.com",
+        url:"mailto:funnydustobusiness@gmail.com"
+    },
+    {
+        label:"twitter/x",
+        subLabel:"@funnydusto",
+        url:"https://x.com/funnydusto",
+    },
+    {
+        label:"discord",
+        subLabel:"dustin's lounge",
+        url:"https://discord.gg/K48QVevPPw",
+    },
+];
+
+const Skills = [
+    {
+        label:"figma",
+        subLabel:"expert"
+    },
+    {
+        label:"framer",
+        subLabel:"intermediate"
+    },
+]
+
+export { Projects };
+export { Connections };
