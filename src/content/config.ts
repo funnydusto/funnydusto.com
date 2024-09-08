89633523d5d09a3422d712c4ea26dbe5bab099ba
funnydusto <dustopersonal@gmail.com>
2024-09-08 09:57:26 -0400

    implementing proper blog props

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,24 @@
 // 1. Import utilities from `astro:content`
 import { z, defineCollection } from 'astro:content';
-import { type SchemaContext } from "astro:content";
 
 // 2. Define a `type` and `schema` for each collection
-const blogCollection = defineCollection({
+const blog = defineCollection({
   type: 'content', // v2.5.0 and later
-  schema: z.object({
+  schema: ({ image }) => z.object({
     title: z.string(),
-    tags: z.array(z.string()),
-    image: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    relatedPosts: z.array(z.string()).optional(),
+    coverLandscape: image().refine((img) => img.width >= 1080, {
+      message: "Cover image must be at least 1080 pixels wide!",
+    }),
+    coverPortrait: image().refine((img) => img.width >= 1080, {
+      message: "Cover image must be at least 1080 pixels wide!",
+    }),
+    seoCover: image().refine((img) => img.width >= 1080, {
+      message: "Cover image must be at least 1080 pixels wide!",
+    }),
+    accentColor: z.string(),
+    isFeatured: z.boolean(),
   }),
 });
 
@@ -30,6 +40,6 @@ const projects = defineCollection({
 
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
-  'blog': blogCollection,
+  'blog': blog,
   'projects': projects,
 };
